de7e4a1ccf765ae91d61dae4e9fdd0802f528df4
Pete Lower <pete.lower@gmail.com>
2015-05-02 11:18:09 -0700

    add base styles and footer wraps

--- footer.php
+++ footer.php
@@ -9,30 +9,28 @@
  * @since Twenty Twelve 1.0
  */
 ?>
+		</div><!-- #inner-main-wrap .inner-wrap -->
 	</div><!-- #main .wrapper -->
 	<footer id="colophon" role="contentinfo">
-		<div class="site-info">
-			
-		</div><!-- .site-info -->
+		<div id="inner-footer-wrap" class="inner-footer-wrap inner-wrap">
+			<?php if ( is_active_sidebar( 'footer-widget-left' ) ) : ?>
+				<div id="footer-widget-left" class="footer-widget-left widget-area" role="complementary">
+					<?php dynamic_sidebar( 'footer-widget-left' ); ?>
+				</div><!-- #footer-widget-left -->
+			<?php endif; ?>
 
-		<?php if ( is_active_sidebar( 'footer-widget-left' ) ) : ?>
-			<div id="footer-widget-left" class="footer-widget-left widget-area" role="complementary">
-				<?php dynamic_sidebar( 'footer-widget-left' ); ?>
-			</div><!-- #footer-widget-left -->
-		<?php endif; ?>
+			<?php if ( is_active_sidebar( 'footer-widget-center' ) ) : ?>
+				<div id="footer-widget-center" class="footer-widget-center widget-area" role="complementary">
+					<?php dynamic_sidebar( 'footer-widget-center' ); ?>
+				</div><!-- #footer-widget-center -->
+			<?php endif; ?>
 
-		<?php if ( is_active_sidebar( 'footer-widget-center' ) ) : ?>
-			<div id="footer-widget-center" class="footer-widget-center widget-area" role="complementary">
-				<?php dynamic_sidebar( 'footer-widget-center' ); ?>
-			</div><!-- #footer-widget-center -->
-		<?php endif; ?>
-
-		<?php if ( is_active_sidebar( 'footer-widget-right' ) ) : ?>
-			<div id="footer-widget-right" class="footer-widget-right widget-area" role="complementary">
-				<?php dynamic_sidebar( 'footer-widget-right' ); ?>
-			</div><!-- #footer-widget-right -->
-		<?php endif; ?>
-		
+			<?php if ( is_active_sidebar( 'footer-widget-right' ) ) : ?>
+				<div id="footer-widget-right" class="footer-widget-right widget-area" role="complementary">
+					<?php dynamic_sidebar( 'footer-widget-right' ); ?>
+				</div><!-- #footer-widget-right -->
+			<?php endif; ?>
+		</div><!-- #inner-footer-wrap -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
